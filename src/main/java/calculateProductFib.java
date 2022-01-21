e64518fcbb00c8cd9304fc67d6f78de3d888d7bf
Isidro <izydemon95@gmail.com>
2022-01-21 11:42:23 +0000

    añadidos

--- src/main/java/calculateProductFib.java
+++ src/main/java/calculateProductFib.java
@@ -4,7 +4,17 @@ public class calculateProductFib {
     }
 
     public int[] productFib(int product){
+        int[] result = new int[3];
+        if(num * num2 == product){
+            result[0] = num;
+            result[1] = num2;
+            result[2] = 1;
+            return result;
+        }
+            return null;
+    }
 
-        return null;
+    private void calculateFibo(int product) {
+
     }
 }
